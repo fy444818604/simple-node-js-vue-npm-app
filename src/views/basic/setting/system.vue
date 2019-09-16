26de92790d38261456a5dfec69c4a1d52f610166
yuan273497548 <yuan273497548>
2019-09-16 17:55:00 +0800

    基础设置里面的系统设置模块以及文件重命名

--- src/views/basic/setting/system.vue
+++ src/views/basic/setting/system.vue
@@ -1,16 +1,39 @@
 <!--系统设置-->
 <template>
 	<div>
-		sadasd
+		<div class="system ">
+			<!--头部-->
+			<div class="system-title clearfix">
+				<div class="fl">111</div>
+				<div class="fr">222</div>
+			</div>
+			<!--名称/logo-->
+			<div class="system-name">
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
 	</div>
 </template>
 
 <script>
     export default {
-        name: "system"
+        name: "system",
+		data(){
+			return{
+
+			}
+		}
     }
 </script>
 
 <style scoped>
-
+	.system{
+		padding: 0px 24px;
+	}
+	.system-title{
+		overflow: hidden;
+		border-bottom: 1px solid #E5E7EF;
+	}
 </style>
