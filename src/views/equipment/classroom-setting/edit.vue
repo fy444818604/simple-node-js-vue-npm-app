b6db917903c6da96b37a63571918ee149b204c3f
忘不了oh <cd0287912@126.com>
2019-09-24 17:26:30 +0800

    li

--- src/views/equipment/classroom-setting/edit.vue
+++ src/views/equipment/classroom-setting/edit.vue
@@ -1,12 +1,142 @@
 <!-- mcu管理添加&编辑 -->
 <template>
-	<div>
-		mcu管理添加&编辑
+	<div class="setting-edit">
+		<div class="header flex">
+			<div class="title">录播教室管理</div>
+			<i class="iconfont icon-arrow-down"></i>
+			<div>1号录播教室</div>
+		</div>
+		<div class="content">
+			<div class="icon">
+				<btn-list v-if="activeName === 'first'" @btn-click="handleAdd" :model="icons.first"></btn-list>
+				<div v-else>
+					<btn-list @btn-click="handleAdd" :model="icons.second[0]"></btn-list>
+					<btn-list @btn-click="handleAdd" :model="icons.second[1]"></btn-list>
+					<btn-list @btn-click="handleAdd" :model="icons.second[2]"></btn-list>
+				</div>
+			</div>
+			<template>
+				<el-tabs v-model="activeName" @tab-click="handleClick">
+					<el-tab-pane label="教室信息" name="first">
+						<ul class="class-info">
+							<li class="item">
+								<span>学校:</span>
+								<span>成都第X中学</span>
+							</li>
+							<li class="item">
+								<span>教室:</span>
+								<span>101/1号教学楼/城南校区</span>
+							</li>
+							<li class="item">
+								<span>名称:</span>
+								<span>1号录播教室</span>
+							</li>
+							<li class="item">
+								<span>描述:</span>
+								<span>1号录播教室</span>
+							</li>
+						</ul>
+					</el-tab-pane>
+					<el-tab-pane label="设备信息" name="second">
+						<div class="equipment-info flex">
+							<div class="left">
+								<template v-if="false">
+									<span>暂无设备信息</span>
+								</template>
+								<template v-else>
+									d
+								</template>
+							</div>
+							<div class="right">2</div>
+						</div>
+					</el-tab-pane>
+				</el-tabs>
+			</template>
+		</div>
 	</div>
 </template>
 
 <script>
+export default {
+	components:{
+		btnList:()=>import("@/components/btn-list"),
+	},
+	data(){
+		return {
+			icons:{first:{icon:'icon-edit',name:'添加'},second:[
+				{icon:'icon-add',name:'添加'},{icon:'icon-edit',name:'编辑'},
+				{icon:'icon-stop',name:'停用'}
+			]},
+			activeName:'first',
+		}
+	},
+	methods:{
+		handleClick(){},
+		handleAdd(){}
+	}
+}
 </script>
 
 <style lang="scss" scoped="scoped">
+.setting-edit {
+	padding: 18px 24px;
+	height: 100%;
+}
+.header{
+	align-items: center;
+	color: #303133;
+	i {
+		font-size: 12px;
+	}
+	.title {
+		color: #303133;
+		font-weight: bold;
+	}
+}
+.content {
+	margin-top: 10px;
+	position: relative;
+	height:calc(100% - 70px);
+	.icon {
+		z-index: 999;
+		position: absolute;
+		right: 0;
+		top: 10px;
+	}
+
+	.class-info {
+		.item {
+			line-height: 36px;
+			margin-top: 20px;
+			color: #606266;
+			span{
+				&:first-child{
+					display: inline-block;
+					width: 60px;
+				}
+			}
+		}
+	}
+	.equipment-info {
+		height: 100%;
+		.left {
+			width: 220px;
+			border-right: 1px solid #E5E7EF;
+		}
+		.right {
+			flex: 1;
+		}
+	}
+}
+</style>
+<style>
+.setting-edit .el-tabs__content {
+	height: 100%;
+}
+.setting-edit .el-tabs {
+	height: 100%;
+}
+.setting-edit .el-tabs  #pane-second {
+	height: 100%;
+}
 </style>
--- src/views/equipment/classroom-setting/index.vue
+++ src/views/equipment/classroom-setting/index.vue
@@ -1,12 +1,82 @@
 <!-- 录播教室管理管理列表 -->
 <template>
-	<div>
-		录播教室管理管理列表
+	<div class="setting">
+		<div class="header flex-between">
+			<div class="left flex">
+				<div class="title">录播教室管理</div>
+				<slelct-tree currentSelect="xxxx" :treeList="treeList"></slelct-tree>
+			</div>
+			<div class="right">
+				<btn-list @btn-click="handleAdd" :model="icons[0]"></btn-list>
+				<btn-list @btn-click="handleAdd" :model="icons[1]"></btn-list>
+				<btn-list @btn-click="handleAdd" :model="icons[2]"></btn-list>
+			</div>
+		</div>
+		<div class="content">
+			 <el-table :data="customData" style="width: 100%">
+				<el-table-column
+					type="selection"
+					width="55">
+				</el-table-column>
+				 <el-table-column width="55" prop="id" align="center" label="序号"></el-table-column>
+				 <el-table-column prop="name" align="center" label="名称"></el-table-column>
+				 <el-table-column prop="class" align="center" label="教室"></el-table-column>
+				 <el-table-column prop="desc" align="center" label="描述"></el-table-column>
+				 <el-table-column prop="status" align="center" label="状态"></el-table-column>
+				 <el-table-column align="center" label="操作">
+					 <template slot-scope="scope">
+						 <div>
+							  <i class="iconfont icon-more"></i>
+						 </div>
+					 </template>
+				 </el-table-column>
+			 </el-table>
+		</div>
+		<div class="footer flex-between">
+			<state-switch></state-switch>
+			<page :pageTotal="20"></page>
+		</div>
 	</div>
 </template>
 
 <script>
+export default {
+	components:{
+		stateSwitch:()=>import("@/components/state-switch"),
+		btnList:()=>import("@/components/btn-list"),
+		page:()=>import("@/components/paging"),
+		slelctTree:() => import("./../../../components/select-tree")
+	},
+	data(){
+		return {
+			icons:[{icon:'icon-add',name:'添加'},{icon:'icon-stop',name:'stop'},{icon:'icon-enable',name:'√'}],
+			customData:[				{
+				id:1,
+				name:'xxx',
+				class:'yyy',
+				desc:'180.11',
+				status:'80'
+				}],
+			treeList:[]
+		}
+	},
+	methods:{
+		handleAdd(){}
+	}
+}
 </script>
 
 <style lang="scss" scoped="scoped">
+.setting {
+	padding: 18px 24px;
+}
+.header{
+	.title {
+		color: #303133;
+		font-weight: bold;
+	}
+}
+.content {
+	margin:26px 0;
+}
 </style>
