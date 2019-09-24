af0c15c8e538436478769b72e577a3c5aa1df096
fy <444818604@qq.com>
2019-09-24 14:23:50 +0800

    fu

---
+++ src/views/authorization/action-auth/detail.vue
@@ -0,0 +1,10 @@
+<!-- 功能授权详情 -->
+<template>
+	 <div>功能授权详情</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped="scoped">
+</style>
---
+++ src/views/authorization/action-auth/index.vue
@@ -0,0 +1,10 @@
+<!-- 功能授权 -->
+<template>
+	 <div>功能授权</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped="scoped">
+</style>
