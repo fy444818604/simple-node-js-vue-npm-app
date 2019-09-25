f0c4405d0db1f02dc8af5cd729bba90c7d108535
fy <444818604@qq.com>
2019-09-25 10:41:17 +0800

    路由变更

--- src/views/basic/account/teacher.vue
+++ src/views/basic/account/teacher.vue
@@ -184,18 +184,7 @@
 			}
 		},
 		mounted() {
-			this.$layer.msg('你确定你很帅么？', {
-				time: 0 //不自动关闭
-					,
-				btn: ['必须啊', '丑到爆'],
-				yes: function(index) {
-					layer.close(index);
-					layer.msg('雅蠛蝶 O.o', {
-						icon: 6,
-						btn: ['嗷', '嗷', '嗷']
-					});
-				}
-			});
+
 		},
 		methods: {
 			btnClick(val) {
