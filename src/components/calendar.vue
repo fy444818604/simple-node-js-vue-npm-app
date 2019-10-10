a97028bca35fcb16a29cc0ffd38fbf3177ac1e27
fy <444818604@qq.com>
2019-10-10 16:40:21 +0800

    fu

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -4,7 +4,7 @@
 		<thead>
 			<tr>
 				<td>{{new Date(current).getFullYear()}}</td>
-				<td v-for="(item,index) in 7">
+				<td v-for="(item,index) in 7" :class="timeWeek == item&&isWeek()?'date-active':''">
 					<p>{{weekDays[index]}}</p>
 					<p>{{weekList[index]}}</p>
 				</td>
@@ -14,8 +14,13 @@
 			<tr v-for="(item,index) in num">
 				<td>第{{index+1}}节</td>
 				<td v-for="(item1,index1) in 7">
-					<div class="count">
-						
+					<div class="count" v-if="isData(item,item1)" :style="{'background-color':`rgba(76,132,255,${0.1*(isData(item,item1).record + isData(item,item1).syn + isData(item,item1).meeting)})`}">
+						{{isData(item,item1).record + isData(item,item1).syn + isData(item,item1).meeting}}
+						<ul>
+							<li>录播:{{isData(item,item1).record}}</li>
+							<li>同步:{{isData(item,item1).syn}}</li>
+							<li>会议:{{isData(item,item1).meeting}}</li>
+						</ul>
 					</div>
 				</td>
 			</tr>
@@ -47,6 +52,19 @@ export default {
         nowTime() {
             let date = new Date();
             return date;
+        },
+        timeWeek:function(){
+            let index;
+            switch (this.nowTime.getDay()){
+            case 0:index = 7;break;
+            case 1:index = 1;break;
+            case 2:index = 2;break;
+            case 3:index = 3;break;
+            case 4:index = 4;break;
+            case 5:index = 5;break;
+            case 6:index = 6;break;
+            }
+            return index;
         }
     },
     methods: {
@@ -75,6 +93,15 @@ export default {
                 }
             }
             return value;
+        },
+        isData(para,para1) {
+            let value = false;
+            this.tableData.map(v => {
+                if(v.x == para&&v.y == para1){
+                    value = v;
+                }
+            })
+            return value;
         }
     },
     watch: {
@@ -102,6 +129,10 @@ export default {
 			td {
 				color: #939399;
 				background-color: #F6F8FA;
+				
+				&.date-active{
+					background-color: #EBEEF5;
+				}
 			}
 		}
 		
@@ -119,7 +150,31 @@ export default {
 					.count{
 						width: 100%;
 						height: 100%;
-						background-color: rgba(76,132,255,1);
+						color: #FFFFFF;
+						font-size: 18px;
+						line-height: 51px;
+						position: relative;
+						
+						&:hover ul{
+							display: block;
+						}
+						
+						ul{
+							position: absolute;
+							left: -4px;
+							bottom: 40px;
+							background: linear-gradient(to top, #7297E8, #4C84FF);
+							border-radius: 4px;
+							width: calc(100% + 8px);
+							height: 86px;
+							padding-top: 7px;
+							display: none;
+							
+							li{
+								color: #FFFFFF;
+								line-height: 24px;
+							}
+						}
 					}
 				}
 			}
