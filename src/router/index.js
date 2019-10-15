a440fe90bd1041dad9ebb9d31977be5c2f60ed1b
fy <444818604@qq.com>
2019-10-15 09:40:14 +0800

    fu

--- src/router/index.js
+++ src/router/index.js
@@ -709,6 +709,30 @@ const index = {
 	}]
 }
 
+//个人中心
+const personal = {
+	path: '/personal/layout',
+	name: 'personal-layout',
+	component: resolve => {
+	    require(['../views/personal/layout/layout'], resolve)
+	},
+	meta: {
+	    title: '首页',
+	    requireAuth: true
+	},
+	children:[{
+		path: '/personal/layout/index',
+		name: 'personal-index',
+		component: resolve => {
+		    require(['../views/personal/person/index'], resolve)
+		},
+		meta: {
+		    title: '个人中心',
+		    requireAuth: true
+		}
+	}]
+}
+
 
 const router = new Router({
     base: './',
@@ -753,7 +777,8 @@ const router = new Router({
 		        title: '通知&新闻详情',
 		        requireAuth: true
 		    }
-		}
+		},
+		personal
     ]
 })
 
---
+++ src/views/personal/layout/layout.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="layout">
+		<index-head></index-head>
+		<div class="main">
+			<router-view></router-view>
+		</div>
+	</div>
+</template>
+
+<script>
+import head from '../../../components/index-head'
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        'index-head': head
+    }
+}
+</script>
+
+<style lang="scss" scoped="scoped">
+</style>
