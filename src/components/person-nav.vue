7b6ec134f7fe9b4734ab581caf776ff38ecdaf42
忘不了oh <cd0287912@126.com>
2019-10-15 19:11:32 +0800

    li

---
+++ src/components/person-nav.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="person-nav">
+    <div class="person-info">
+      <img class="avatar" src="http://cdn.admui.com/demo/pjax/2.0.0/images/avatar.svg" alt="">
+      <div class="name">xxx</div>
+      <div class="school">
+        <p>成都市泡桐树小学天府校区</p>
+        <p>(泡小天府校区)</p>
+      </div>
+      <div class="contact">
+        <div class="attention">
+          <p>120</p>
+          <span>关注</span>
+        </div>
+        <div class="fans">
+          <p>120</p>
+          <span>粉丝</span>
+        </div>
+      </div>
+    </div>
+    <ul class="nav-list">
+      <li>个人中心</li>
+      <li>资源</li>
+      <li>专辑</li>
+      <li>预约</li>
+      <li>留言板</li>
+      <li>收藏</li>
+      <li>设置</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.person-nav {width: 100%;height: 100%;
+background: #fff;
+box-shadow: 0px 5px 20px 0px rgba(26,27,27,0.05);
+}
+.person-nav .person-info {
+  height: 270px;display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #E5E5E5;
+  padding: 30px;
+  padding-bottom: 0;
+  align-items: center;}
+.person-info img.avatar {width: 80px;}
+.person-info .name {color: #333;font-size: 16px}
+.person-info .school {text-align: center;p{color: #909399;}}
+.person-info .contact {margin-top: 20px;width:100%;display: flex;justify-content: space-between}
+
+.person-nav .nav-list li {
+  height: 48px;color: #606266;
+  text-align: center;
+  line-height: 48px;
+  cursor: pointer;
+  }
+  .person-nav .nav-list li:hover {
+    background: #2695FF;
+    color: #fff;
+  }
+</style>
--- src/views/personal/layout/layout.vue
+++ src/views/personal/layout/layout.vue
@@ -1,19 +1,21 @@
 <template>
 	<div id="layout">
 		<index-head></index-head>
-		<div class="main">
-			<div class="nav-wrap">
-				
-			</div>
-			<div class="container">
-				<router-view></router-view>
-			</div>
-		</div>
+    <div class="container">
+      <div class="nav-wrap">
+        <personNav></personNav>
+      </div>
+      <el-scrollbar>
+        <router-view></router-view>
+      </el-scrollbar>
+    </div>
+
 	</div>
 </template>
 
 <script>
 import head from '../../../components/index-head'
+import personNav from '../../../components/person-nav'
 export default {
     data() {
         return {
@@ -21,22 +23,20 @@ export default {
         }
     },
     components: {
-        'index-head': head
+        'index-head': head,
+        personNav
     }
 }
 </script>
 
 <style lang="scss" scoped="scoped">
-	.main{
-		min-height: calc(100vh - 60px);
-		display: flex;
-		
-		.nav-wrap{
-			width: 240px;
-		}
-		
-		.container{
-			width: calc(100% - 240px);
-		}
-	}
+.container {
+  height: calc(100vh - 60px);
+  display: flex;
+  background: #F5F7FB;
+  .nav-wrap {
+    width: 240px;
+    flex-shrink: 0;
+  }
+}
 </style>
